3cefbd01783341b6d506c99399f80c11cc0f20c7
coweaver <cooperweaver@mac.com>
2015-05-01 14:06:12 -0400

    semi-working chat

--- message.js
+++ message.js
@@ -1,7 +1,14 @@
 console.log("HELLO");
-var strings ["Hi", "How are you?", "good"];
+var strings = ["Hi", "How are you?", "good"];
 
 var block = d3.select("#block");
-for(var i = 0; i < strings.length(); i++){ 
-    block.text(strings[i]);
+//for(var i = 0; i < strings.length; i++){
+//    block.text(strings[0]);
+//}
+
+function send(message){
+    block.append("").text(message);
+    document.getElementById('textbox1').value = "";
+    objDiv = document.getElementById("block");
+    objDiv.scrollTop = objDiv.scrollHeight;
 }
