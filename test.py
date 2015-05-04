f83b72c151794f1e8ff46f7f9550e248dc65617b
Kyler Chase <kylerchase@earthlink.net>
2015-05-04 13:15:03 -0400

    added base for messages, walls, edited test

--- test.py
+++ test.py
@@ -2,9 +2,18 @@ from flask import Flask, flash, session, request, url_for, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
 @app.route("/home", methods = ["POST", "GET"])
 def home():
     return render_template("home.html")
+
+@app.route("/messages", methods = ["POST", "GET"])
+def messages():
+    return render_template("messages.html")
+
+@app.route("/walls", methods = ["POST", "GET"])
+def walls():
+    return render_template("walls.html")
 
 
 if __name__=="__main__":
