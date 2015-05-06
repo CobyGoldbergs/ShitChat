eb96a902b88411fe96bac8e0814680ac61f6e7c7
Kyler Chase <kylerchase@earthlink.net>
2015-05-06 13:17:22 -0400

    made register, played with login

--- test.py
+++ test.py
@@ -23,6 +23,10 @@ def create():
 def login():
     return render_template("login.html")
 
+@app.route("/register", methods = ["POST", "GET"])
+def register():
+    return render_template("register.html")
+
 
 if __name__=="__main__":
     app.debug = True
