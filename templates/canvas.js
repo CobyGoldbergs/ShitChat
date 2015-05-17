948bdd69b5a341ad4f85d924f9669d3b08872a51
JulianKal <jkalogerakis8@gmail.com>
2015-05-17 19:12:26 -0400

    multicolor doodles

--- templates/canvas.js
+++ templates/canvas.js
@@ -2,14 +2,30 @@ var canvas = document.getElementById('c');
 var ctx = canvas.getContext('2d');
 var radius=0;
 var marker = document.getElementById("marker");
-var eraser = document.getElementById("eraser");
+redMarker = document.getElementById("red");
+blueMarker = document.getElementById("blue");
+greenMarker = document.getElementById("green");
+var redMarker;
+var blueMarker;
+var greenMarker;
+var eraser;
+
 var editButton = document.getElementById("edit");
 var mouseDown = 0;
+
 var markerSelected = false;
+var redSelected = false;
+var blueSelected = false;
+var greenSelected = false;
+
 var eraserSelected = false;
+
 var canEdit = false;
 var whiteEdits = [];
 var redEdits = [];
+var blueEdits = [];
+var greenEdits = [];
+
 document.body.onmousedown = function() { 
     ++mouseDown;
 }
@@ -18,7 +34,6 @@ document.body.onmouseup = function() {
 }
 window.onload = function() {
     ctx.fillStyle ="rgba(0, 10, 25, 0)";
-    ctx.canvas.width = window.innerWidth - 100;
 };
 
 draw = function (e) { 
@@ -27,9 +42,17 @@ draw = function (e) {
     ctx.arc(e.offsetX, e.offsetY, radius, 0,2*Math.PI);
     ctx.fill();
     //stores red and white circle data
-    if(markerSelected){
-        console.log("(" + e.offsetX + ", " + e.offsetY + ") --- marker");
+    if(redSelected){
+        console.log("(" + e.offsetX + ", " + e.offsetY + ") --- red marker");
         redEdits[redEdits.length] = [e.offsetX, e.offsetY];
+    }
+    if(blueSelected){
+        console.log("(" + e.offsetX + ", " + e.offsetY + ") --- blue marker");
+        blueEdits[redEdits.length] = [e.offsetX, e.offsetY];
+    }
+    if(greenSelected){
+        console.log("(" + e.offsetX + ", " + e.offsetY + ") --- green marker");
+        greenEdits[redEdits.length] = [e.offsetX, e.offsetY];
     }
     if(eraserSelected){
         console.log("(" + e.offsetX + ", " + e.offsetY + ") --- eraser");
@@ -37,18 +60,61 @@ draw = function (e) {
     }
 }
 
+
 marker.addEventListener("click",function(e){
     ctx.fillStyle ="rgba(255, 0, 0, 1.0)";
-    radius= 7;
-    eraserSelected = false;
-    markerSelected = true;
+    radius= 4;
+    document.getElementById("tools").innerHTML = document.getElementById("tools").innerHTML 
+                                                + "<img src='http://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Circle-green.svg/512px-Circle-green.svg.png' width='50' id='green'>" 
+                                                + "<img src ='http://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Circle-red.svg/512px-Circle-red.svg.png' width='50' id='red'>" 
+                                                + "<img src ='http://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Circle-blue.svg/2000px-Circle-blue.svg.png' width='50' id='blue'>";
+    try{
+        redMarker = document.getElementById("red");
+        blueMarker = document.getElementById("blue");
+        greenMarker = document.getElementById("green");
+        eraser = document.getElementById("eraser");
+        redMarker.addEventListener("click",function(e){
+            ctx.fillStyle ="rgba(255, 0, 0, 1.0)";
+            radius= 4;
+            eraserSelected = false;
+            markerSelected = true;
+            redSelected = true;
+            blueSelected = false;
+            greenSelected = false;
+
+        });
+        blueMarker.addEventListener("click",function(e){
+            ctx.fillStyle ="rgba(0, 0, 255, 1.0)";
+            radius= 4;
+            eraserSelected = false;
+            markerSelected = true;
+            redSelected = false;
+            blueSelected = true;
+            greenSelected = false;
+        });
+        greenMarker.addEventListener("click",function(e){
+            ctx.fillStyle ="rgba(0, 255, 0, 1.0)";
+            radius= 4;
+            eraserSelected = false;
+            markerSelected = true;
+            redSelected = false;
+            blueSelected = false;
+            greenSelected = true;
+        });
+        eraser.addEventListener("click",function(e){
+            ctx.fillStyle ="rgba(255, 255, 255, 1.0)";
+            radius= 20;
+            markerSelected = false;
+            redSelected = false;
+            blueSelected = false;
+            greenSelected = false;
+            eraserSelected = true;
+        });
+
+    }catch(e){}
 });
-eraser.addEventListener("click",function(e){
-    ctx.fillStyle ="rgba(255, 255, 255, 1.0)";
-    radius= 20;
-    markerSelected = false;
-    eraserSelected = true;
-});
+    
+
 editButton.addEventListener("click",function(e){
     if(!canEdit){
         document.getElementById("edit").innerHTML = "Now Editing - Click Here To Save";
@@ -64,7 +130,6 @@ editButton.addEventListener("click",function(e){
     }
     canEdit = !canEdit;
 });
-
 
 
 canvas.addEventListener('mousemove', function(e) {
